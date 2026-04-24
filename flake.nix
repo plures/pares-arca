2025114bcee636a6786eab0585797c1592216b91
Kayode Bristol <kayode.bristol@gmail.com>
2026-04-24 18:34:59 +0000

    fix: sync flake.nix version to 0.2.1 to match Cargo.toml

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
       {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "pares-cache";
-          version = "0.1.0";
+          version = "0.2.1";
           src = ./.;
           cargoLock.lockFile = ./Cargo.lock;
           nativeBuildInputs = with pkgs; [ pkg-config ];
